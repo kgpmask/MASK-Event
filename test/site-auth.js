1a38a996a5fa9e397c25551c0a504d14fcd140c1
Goose <64352864+Goose-Of-War@users.noreply.github.com>
2023-07-20 21:44:48 +0530

    Login tests

--- test/site-auth.js
+++ test/site-auth.js
@@ -1,55 +1,63 @@
-const axios = require('axios');
-const assert = require('assert');
-const DB = require('../database/database');
-const dbh = require('../database/handler');
 const PORT = 7359;
+process.env.PORT = PORT;
 global.PARAMS = { test: true };
 
-let db;
+const assert = require('assert');
+const axios = require('axios');
 
-before(async function () {
-	db = await DB.init();
-});
+const server = require('../src/server');
+const dbh = require('../database/handler');
 
-describe('Database connection', () => {
-	it('should be connected', () => assert(db.connection.readyState));
-});
+describe('Server (auth):', () => {
+	before(async () => await server.ready());
 
-describe('User', () => {
+	it('Should have the right PARAMS object', () => assert.deepEqual(PARAMS, { test: true }));
 
-	it('should be able to register a test user', async function () {
-		await axios.post(`http://localhost:${PORT}/signup`,
-			{
-				_id: 6969,
-				name: 'goos',
-				roll: '23XX69420',
-				username: 'deadgoos',
-				phone: '1234567890',
-				email: 'deadgoos@kgpmask.club',
-				password: 'AsIfImmaLeakMyPassword'
-			}
-		).then(res => {
+	describe('Should prevent signup in case of missing data', () => {
+		it('Blank Username', () => axios.post(`http://localhost:${PORT}/login`,
+			{ username: '   ', password: 'random' }
+		).then(() => Promise.resolve(false)).catch(({ response }) => {
+			assert(response.status === 500);
+			assert(response.data === 'Error: No Username Provided');
+		})
+		).timeout(1_000);
+
+		it('Blank Password', () => axios.post(`http://localhost:${PORT}/login`,
+			{ username: 'random', password: '   ' }
+		).then(() => Promise.resolve(false)).catch(({ response }) => {
+			assert(response.status === 500);
+			assert(response.data === 'Error: No Password Provided');
+		})
+		).timeout(1_000);
+	});
+
+	describe('Should not login for invalid credentials', () => {
+		it('Invalid Username', () => axios.post(`http://localhost:${PORT}/login`,
+			{ username: 'test', password: 'random' }
+		).then(() => Promise.resolve(false)).catch(({ response }) => {
+			assert(response.status === 500);
+			assert(response.data === 'Error: User could not be found');
+		})
+		).timeout(process.platform === 'win32' ? 5_000 : 3_000);
+
+		it('Blank Username', () => axios.post(`http://localhost:${PORT}/login`,
+			{ username: 'testuser', password: 'random' }
+		).then(() => Promise.resolve(false)).catch(({ response }) => {
+			assert(response.status === 500);
+			assert(response.data === 'Error: Password does not match');
+		})
+		).timeout(process.platform === 'win32' ? 5_000 : 3_000);
+	});
+
+	describe('Should login to test user successfully', () => it('With test user credentials',
+		() => axios.post(`http://localhost:${PORT}/login`,
+			{ username: 'testuser', password: 'password' }
+		).then(async res => {
 			assert(res.status === 200);
-			assert(res.data === 'Registered Successfully UwU');
-		});
-	}
-	).timeout(process.platform === 'win32' ? 5_000 : 3_000);
-
-	it('should login to test user', () => axios.post(`http://localhost:${PORT}/login`,
-		{ username: 'dedgoos', password: 'AsIfImmaLeakMyPassword' }
-	).then(async res => {
-		// console.log(res);
-		assert(res.status === 200);
-		assert(res.data === 'logged in >w<');
-		const sessionID = res.headers['set-cookie'].pop().split(';').filter(i => i.includes('sessionID=')).pop().slice(10);
-		assert.deepEqual(await dbh.getUserFromSessionID(sessionID), await dbh.getUserByUsername('dedgoos'));
-	})
-	).timeout(process.platform === 'win32' ? 5_000 : 3_000);
-});
-
-
-
-after(async function () {
-	await dbh.removeTestUser;
-	DB.disconnect();
+			const sessionID = res.headers['set-cookie'].pop().split(';').filter(i => i.includes('sessionID=')).pop().slice(10);
+			assert.deepEqual(await dbh.getUserFromSessionID(sessionID), await dbh.getUserByUsername('testuser'));
+		})
+	).timeout(process.platform === 'win32' ? 5_000 : 3_000)
+	);
+	after(server.close);
 });
