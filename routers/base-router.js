a2a3f1e2ea7f64262055d8788d49fcb98209973d
Goose <64352864+Goose-Of-War@users.noreply.github.com>
2023-07-20 15:50:29 +0530

    Forgot to change this

--- routers/base-router.js
+++ routers/base-router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.use('/', (req, res) => res.renderFile('auth/login.njk'));
+router.use('/', (req, res) => res.renderFile('home.njk'));
 
 module.exports = {
 	route: ['/home', '/'],
