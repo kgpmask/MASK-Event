51c34479bde398a985cd5792b5a0a93ca8dd764f
Symbiot01 <sahil0111patel@gmail.com>
2023-07-20 22:01:37 +0530

    Why am i doing this

--- routers/base-router.js
+++ routers/base-router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.use('/', (req, res) => res.renderFile('home.njk'));
+router.use('/', (req, res) => res.renderFile('auth/signup.njk'));
 
 module.exports = {
 	route: ['/home', '/'],
