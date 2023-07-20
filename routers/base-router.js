08690f7622538263fcb430e3250cb24c16aa73f9
Symbiot01 <sahil0111patel@gmail.com>
2023-07-21 01:00:46 +0530

    Added links in forms

--- routers/base-router.js
+++ routers/base-router.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-router.use('/', (req, res) => res.renderFile('auth/signup.njk'));
+router.use('/', (req, res) => res.renderFile('auth/login.njk'));
 
 module.exports = {
 	route: ['/home', '/'],
