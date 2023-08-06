844842c63f3f8ef2bc59629cb0c8910168f66f94
SachdevJai <jaisachdev@kgpian.iitkgp.ac.in>
2023-08-06 15:31:28 +0530

    timer

--- routers/live-router.js
+++ routers/live-router.js
@@ -1,8 +1,26 @@
 const router = require('express').Router();
+const dbh = require('../database/handler');
 
-// app.get('/master', async (req, res) => {
-// 	if (PARAMS.dev) {
-// 		// TODO: In the future, set a 'daily' script to run at midnight and update a process.env.LIVE_QUIZ parameter
+app.get('/master', async (req, res) => {
+	// TODO: In the future, set a 'daily' script to run at midnight and update a process.env.LIVE_QUIZ parameter
+	const quiz = await dbh.getLiveQuiz('SQ1');
+	// if (!quiz) return res.renderFile('events/quizzes_404.njk', { message: `The quiz hasn't started, yet!` });
+	const questions = quiz.questions;
+	return res.renderFile('live/master.njk', {
+		quiz: JSON.stringify(questions),
+		qAmt: questions.length,
+		id: 'live',
+		dev: PARAMS.dev
+	});
+});
+
+// router.get('/', (req, res) => {
+// 	// if (req.user.isAdmin) return res.renderFile('live/master.njk');
+// 	res.renderFile('live/participant.njk');
+// });
+
+// router.get('/', async (req, res) => {
+// 	if (req.user.isAdmin) {
 // 		const quiz = await dbh.getLiveQuiz();
 // 		// if (!quiz) return res.renderFile('events/quizzes_404.njk', { message: `The quiz hasn't started, yet!` });
 // 		const questions = quiz.questions;
@@ -13,30 +31,9 @@ const router = require('express').Router();
 // 			dev: PARAMS.dev
 // 		});
 // 	} else {
-// 		return res.renderFile('events/quizzes_404.njk', { message: `STOP SNOOPING AROUND!` });
+// 		return res.renderFile('live/participant.njk');
 // 	}
 // });
-
-// router.get('/', (req, res) => {
-// 	// if (req.user.isAdmin) return res.renderFile('live/master.njk');
-// 	res.renderFile('live/participant.njk');
-// });
-
-router.get('/', async (req, res) => {
-	if (req.user.isAdmin) {
-		const quiz = await dbh.getLiveQuiz();
-		// if (!quiz) return res.renderFile('events/quizzes_404.njk', { message: `The quiz hasn't started, yet!` });
-		const questions = quiz.questions;
-		return res.renderFile('events/live_master.njk', {
-			quiz: JSON.stringify(questions),
-			qAmt: questions.length,
-			id: 'live',
-			dev: PARAMS.dev
-		});
-	} else {
-		return res.renderFile('live/participant.njk');
-	}
-});
 
 router.post('/submit', (req, res) => {
 	if (req.user.isAdmin) return res.send('admins are not allowed here ;-;');
