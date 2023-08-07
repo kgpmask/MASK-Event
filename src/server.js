3de48f6c435259321f464e87d1e3dcffd1e9f1ef
SachdevJai <jaisachdev@kgpian.iitkgp.ac.in>
2023-08-07 18:23:29 +0530

    added stuff in server.js

--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ const PORT = process.env.PORT ?? 6970;
 const initMiddleware = require('./middleware');
 const router = require('./router');
 const DB = require('../database/database');
+const socketio = require('socket.io')();
 
 global.app = express();
 const waitForDB = PARAMS.mongoless ? Promise.resolve({}) : DB.init();
@@ -29,6 +30,9 @@ const server = http.createServer(app);
 server.listen(PORT, () => {
 	if (!PARAMS.test) console.log(`The server is up at http://localhost:${PORT}/`);
 });
+
+global.io = socketio.listen(server);
+require('./socket');
 
 exports.ready = () => waitForDB;
 exports.close = () => {
