c1cac9ebab7aa59bc9c4e97e596faf2b14cac48d
SachdevJai <jaisachdev@kgpian.iitkgp.ac.in>
2023-08-10 22:31:12 +0530

    router admin stuf

--- routers/live-router.js
+++ routers/live-router.js
@@ -4,28 +4,40 @@ const dbh = require('../database/handler');
 const handlerContext = {};
 
 router.post('/startQ', (req, res) => {
-	const qNum = req.body.questionNumber;
-	const currentQ = handlerContext.quiz.questions[qNum].question;
-	const options = handlerContext.quiz.questions[qNum].options;
-	const type = handlerContext.quiz.questions[qNum].type;
-	console.log(currentQ, options, type);
-	io.sockets.in('waiting-for-live-quiz').emit('question', { currentQ, type, options });
-	handlerContext.LQnum = qNum;
-	res.send('question-live');
+	if (req.user.isAdmin) {
+		const qNum = req.body.questionNumber;
+		const currentQ = handlerContext.quiz.questions[qNum].question;
+		const options = handlerContext.quiz.questions[qNum].options;
+		const type = handlerContext.quiz.questions[qNum].type;
+		console.log(currentQ, options, type);
+		io.sockets.in('waiting-for-live-quiz').emit('question', { currentQ, type, options });
+		handlerContext.LQnum = qNum;
+		return res.send('question-live');
+	} else {
+		return res.send('not admin');
+	}
 });
 
 router.post('/start-quiz', (req, res) => {
-	const quizId = req.body.id;
-	io.sockets.in('waiting-for-live-quiz').emit('start', true);
-	return res.send('quiz-started');
+	if (req.user.isAdmin) {
+		io.sockets.in('waiting-for-live-quiz').emit('start', true);
+		return res.send('quiz-started');
+	} else {
+		return res.send('not admin');
+	}
 });
 
 router.post('/end-quiz', (req, res) => {
-	io.sockets.in('waiting-for-live-quiz').emit('end', true);
-	res.send('quiz-ended');
+	if (req.user.isAdmin) {
+		io.sockets.in('waiting-for-live-quiz').emit('end', true);
+		res.send('quiz-ended');
+	} else {
+		return res.send('not admin');
+	}
 });
 
 router.get('/', async (req, res) => {
+	if (!req.user) return res.send('not logged in');
 	if (req.isAdmin) {
 		handlerContext.quiz = await dbh.getLiveQuiz('SQ4');
 		// if (!quiz) return res.renderFile('events/quizzes_404.njk', { message: `The quiz hasn't started, yet!` });
@@ -36,7 +48,7 @@ router.get('/', async (req, res) => {
 			id: 'live'
 		});
 	} else {
-		if (!Object.keys(handlerContext).length) return res.renderFile('live/landing.njk');
+		// if (!Object.keys(handlerContext).length) return res.renderFile('live/landing.njk');
 		return res.renderFile('live/participant.njk');
 	}
 });
