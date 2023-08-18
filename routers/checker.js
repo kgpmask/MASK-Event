22d26016245ece54cc3a9dbf7e80210ebd8a518e
Ankan <ankansahadgp@gmail.com>
2023-08-18 18:45:39 +0530

    redundant checker

--- routers/checker.js
+++
@@ -1,9 +0,0 @@
-const checker = require('../src/checker.js');
-const dbh = require('../database/handler');
-
-exports.check = async function check (responses, type, solution) {
-	Object.entries(responses).map(async ([userId, answer] = response) => {
-		const points = checker.checkLive(answer, type, solution);
-		if (points) return await dbh.updateLiveResult(userId, points);
-	});
-};
--- routers/live-router.js
+++ routers/live-router.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 const dbh = require('../database/handler');
 
-const check = require('./checker');
+const checker = require('../src/checker.js');
 
 const handlerContext = {};
 
@@ -45,10 +45,48 @@ router.post('/start-q', (req, res) => {
 		io.sockets.in('waiting-for-live-quiz').emit('answer');
 		Object.entries(handlerContext.responseCache).map(async ([userId, answer] = response) => {
 			await dbh.addLiveRecord(userId, quizID, handlerContext.qNum, answer);
+			const points = checker.checkLive(answer, type, solution);
+			if (points) return await dbh.updateLiveResult(userId, points);
 		});
 		check.check(handlerContext.responseCache, type, handlerContext.quiz.questions[qNum].solution);
 	}, 23000);
 	return res.send('question-live');
+});
+
+router.post('/start-quiz', (req, res) => {
+	if (req.user.isAdmin) {
+		io.sockets.in('waiting-for-live-quiz').emit('start', true);
+		return res.send('quiz-started');
+	} else {
+		return res.send('not admin');
+	}
+});
+
+router.post('/end-quiz', (req, res) => {
+	if (req.user.isAdmin) {
+		io.sockets.in('waiting-for-live-quiz').emit('end', true);
+		return res.send('quiz-ended');
+	} else {
+		return res.send('not admin');
+	}
+});
+
+router.get('/', async (req, res) => {
+	if (!req.user) return res.send('not logged in');
+	if (req.isAdmin) {
+		handlerContext.quiz = await dbh.getLiveQuiz(quizID);
+		// if (!quiz) return res.renderFile('events/quizzes_404.njk', { message: `The quiz hasn't started, yet!` });
+		const questions = handlerContext.quiz.questions;
+		return res.renderFile('live/master.njk', {
+			questions,
+			qAmt: questions.length,
+			id: 'live'
+		});
+	} else {
+		if (!Object.keys(handlerContext).length) return res.renderFile('info/landing.njk');
+		return res.renderFile('live/participant.njk');
+	}
+});
 
 });
 
