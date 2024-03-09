136a33d92164b4e1452fdb67bfc81b43e5cc7e5a
Ankan <93087057+ItsAnkan@users.noreply.github.com>
2024-03-09 22:13:16 +0530

    nvm i get it ;-;

--- routers/live-router.js
+++ routers/live-router.js
@@ -59,7 +59,7 @@ router.post('/get-attempted', (req, res) => {
 router.post('/get-state', (req, res) => {
 	const teamID = req.body.teamID;
 	if (teamID !== req.team._id) return res.status(500).send('WHy you hax');
-	return res.status(200).send(req.team.state);
+	return res.status(200).send(req.team.status);
 });
 router.post('/get-timeout', async (req, res) => {
 	if (req.team.status === 'riddle-timeout') {
