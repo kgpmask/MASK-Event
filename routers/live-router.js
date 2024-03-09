63d90ad6b361e0972267e7836f0d2d8b905291a6
destryptor <sharanya.chakraborty@kgpian.iitkgp.ac.in>
2024-03-09 21:55:51 +0530

    Onkon's sentence increased

--- routers/live-router.js
+++ routers/live-router.js
@@ -65,11 +65,11 @@ router.post('/get-timeout', async (req, res) => {
 
 router.post('/update-status', async (req, res) => {
 	await dbh.updateTeamStatus({ _id: req.team._id, status: req.body.status, questionNo: req.body.questionNo });
-	return req.status(200).send('Updated Successfully');
+	return res.status(200).send('Updated Successfully');
 });
 router.post('/update-attempted', async (req, res) => {
 	await dbh.updateTeamStatus({ _id: req.team._id, status: req.body.status, questionNo: req.body.questionNo });
-	return req.status(200).send('Updated Successfully');
+	return res.status(200).send('Updated Successfully');
 });
 
 // router.patch('/location-code', async (req, res) => {
